fe62fccb370349df8577c62d9db5f8f315f69e7b
naomi.solomons <ua19167@bristol.ac.uk>
2020-12-07 17:36:43 +0000

    Adding the plotting feature for random cov matrix

--- fivemode.py
+++ fivemode.py
@@ -4,12 +4,16 @@ from itertools import product
 import copy
 import math
 import cmath
+import random as rand
+#from bristol import ensembles
+import matplotlib.pyplot as plt
 
 #User inputs
 
 covariance0 = np.zeros((10,10))
 
 displacement0 = np.zeros(10)
+#I've taken the displacement vector as an input but it doesn't actually use it
 
 #This is a set of indices
 set0 = np.zeros((5,), dtype=int)
@@ -20,8 +24,6 @@ numbers0 = [1,1,1,1,1]
 ########################################
 
 x_matrix = [[0,1],[1,0]]
-
-
 
 def kappa_new(covariance, displacement, set, numbers):
 
@@ -37,7 +39,7 @@ def kappa_new(covariance, displacement, set, numbers):
         sigma_q: the sigma_q matrix as defined in the notes
     '''
 
-    sigma_s = np.zeros((2*len(set),2*len(set)))
+    sigma_s = np.zeros((2*len(set),2*len(set)), dtype=np.complex_)
 
     #This is the covariance matrix but with only the modes we are interested in
 
@@ -58,7 +60,7 @@ def kappa_new(covariance, displacement, set, numbers):
 
     #All this next bit is how we repeat modes as is necessary based on the p_i values
 
-    kappa_s_geq_1 = np.zeros((2*np.sum(numbers),2*np.sum(numbers)))
+    kappa_s_geq_1 = np.zeros((2*np.sum(numbers),2*np.sum(numbers)), dtype=np.complex_)
 
     position = 0
     position_temp = 0
@@ -82,6 +84,8 @@ def Hafnian(kappa_matrix):
     '''This function returns the Hafnian (float) of a given input matrix (square array).'''
 
     hafnian = 0 #placeholder
+    print('k mat')
+    print(kappa_matrix)
 
     #Probably worth writing in a check that the input is a square matrix and the size is even.
 
@@ -144,29 +148,69 @@ def click_probability(covariance, displacement, set, numbers):
     '''This is going to give us the probability of seeing a particular pattern of photon numbers (this is the list of integers 'numbers')
     over a particular set of modes (this is the list of integers 'set') with the input covariance matrix (must be size 2N x 2N) and
     displacement vector.'''
+    if sum(numbers) < 0:
+        print('Need to work on this case.')
+        probability = 0
+    else:
+        kappa_s_geq_1, sigma_q = kappa_new(covariance, displacement, set, numbers)
 
-    kappa_s_geq_1, sigma_q = kappa_new(covariance, displacement, set, numbers)
+        hafnian = Hafnian(kappa_s_geq_1)
+        N = 1
+        for p_i in numbers:
+            N *= 1/math.factorial(p_i)
 
-    hafnian = Hafnian(kappa_s_geq_1)
-    N = 1
-    for p_i in numbers:
-        N *= 1/math.factorial(p_i)
-
-    probability = ((2**len(set))*N/np.sqrt(np.linalg.det(sigma_q)))*hafnian
+        probability = ((2**len(set))*N/np.sqrt(np.linalg.det(sigma_q)))*hafnian
     return probability
 
+#########################################Here's some examples#############################################
 
-c2 = 3.7622
-s2 = 3.6269
+def covariance_2mode_squeezed(r, phi):
+    c2 = np.cosh(2*r)
+    s2 = np.sinh(2*r)
+    return [[c2, 0, 0,cmath.rect(s2, phi)],[0,c2,cmath.rect(s2, phi),0],[0,cmath.rect(s2, -phi),c2,0],[cmath.rect(s2, -phi),0,0,c2]]
 
-covariance1 = [[c2, 0, 0,s2],[0,c2,s2,0],[0,s2,c2,0],[s2,0,0,c2]]
 set1 = [0,1]
-numbers1 = [2,2]
+numbers1 = [1,1]
+covariance1 = covariance_2mode_squeezed(1, math.pi/6)
 
-print(click_probability(covariance1, displacement0, set1, numbers1))
+#print(click_probability(covariance1, displacement0, set1, numbers1))
 
-#Main issues
-#2. currently only accepts real numbers - could change it for complex numbers or symbols
-#3. I've taken the displacement vector as an input but it doesn't actually use it
+#########################################################################################################
 
-#########################################Here's some states to use as examples#############################################
+#Next step, we are going to try and make some plots with random covariance matrices.
+
+#random_symp = ensembles.Circular().gen_cse(N=5)
+#print(random_symp)
+
+random_A = np.random.rand(5,5) #This is a method from wikipedia for constructing random symplectic matrices
+C_matrix = np.linalg.inv(random_A.transpose())
+D_matrix = np.zeros((10,10))
+D_matrix[:random_A.shape[0],:random_A.shape[1]]=random_A #I took this from stackexchange because there doesn't seem to be a method for direct sum in numpy?
+D_matrix[random_A.shape[0]:,random_A.shape[1]:]=C_matrix #I'm sure I could have worked it out by itself
+random_B = np.random.rand(5,5)
+B_matrix = (random_B + random_B.transpose())/2 #Same here but for symmetrix matrices
+N_matrix = np.identity(10) + np.kron([[0,1],[0,0]], B_matrix)
+Omega_matrix = np.zeros((10,10)) + np.kron([[0,1],[0,0]], np.identity(5)) + np.kron([[0,0],[-1,0]], np.identity(5))
+rand4 = rand.randint(0,3)
+random_symp = np.matmul(np.matmul(np.linalg.matrix_power(Omega_matrix,rand4), D_matrix), N_matrix)
+random_cov = np.matmul(random_symp, random_symp.transpose())
+print(random_cov)
+
+list_of_sets = list(product(list(range(3)),repeat=5))
+sumofprobs = 0
+probabilities = []
+#for i in range(len(list_of_sets)):
+#    print(list_of_sets[i])
+#    sumofprobs += click_probability(random_cov, np.zeros(10), [0,1,2,3,4], list_of_sets[i])
+#    print(click_probability(random_cov, np.zeros(10), [0,1,2,3,4], list_of_sets[i]))
+#    probabilities.append(click_probability(random_cov, np.zeros(10), [0,1,2,3,4], list_of_sets[i]))
+#print(sumofprobs)
+print(click_probability(random_cov, np.zeros(10), [0,1,2,3,4], [0,0,0,0,0]))
+#plt.bar(list(range(len(list_of_sets))), np.real(probabilities))
+#plt.show()
+
+################################TO DO LIST##############################################################
+
+#I need to double check this works when not all modes are selected.
+#It would be good to check that everything is input correctly.
+#Maybe compare this to the Xanadu method?
