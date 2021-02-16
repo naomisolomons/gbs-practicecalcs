e9188b126f45ca3d7b67f567e0eeb76a3bfdee15
Naomi Solomons <ua19167@bristol.ac.uk>
2021-02-16 11:12:08 +0000

    Added file for C++

---
+++ gbs-statistics/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//Step 1: up here I will define the x matrix
+
+int main()
+{
+
+    // The program is going to produce various statistics associated with Gaussian Boson Sampling.
+
+    
+
+
+
+    return 0;
+}
