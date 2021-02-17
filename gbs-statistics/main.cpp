cd29a7d2cce58db43aba0acfb8ce85729b044fe2
Naomi Solomons <ua19167@bristol.ac.uk>
2021-02-17 12:37:05 +0000

    Program now runs but need to change scope of tempmu

--- gbs-statistics/main.cpp
+++ gbs-statistics/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 
-/* int doublefactorial(int n) // This is from the internet (Rahul Agrawal)
+
+int doublefactorial(int n) // This is from the internet (Rahul Agrawal)
 {
     if (n == 0 || n==1)
       return 1;
@@ -10,11 +12,12 @@
 }
 
 
-std::vector<std::vector<int>> recursiveLoop(std::vector<int> indices, int N, std::vector<std::vector<int>> tempmu,
+std::vector<std::vector<int>> recursiveLoop(std::vector<int> indices, int N, std::vector<int> tempmu,
                                             std::vector<std::vector<int>> allmus, int endsize) //Not sure about use of const here
 {
     if (tempmu.size() == N)
     {
+        //std::cout << "We get here";
         allmus.push_back(tempmu);
         tempmu.pop_back();
         tempmu.pop_back();
@@ -38,25 +41,27 @@ std::vector<std::vector<int>> recursiveLoop(std::vector<int> indices, int N, std
         std::vector<int> newlist{};
         for(int i=0; i < indices.size(); i++)
         {
-            if (std::find(tempmu.begin(), tempmu.end(), indices[i]) = tempmu.end())
+            if (std::find(tempmu.begin(), tempmu.end(), indices[i]) == tempmu.end())
                 newlist.push_back(indices[i]);
             else
                 ;
         }
-        for(int i=1, i<newlist.size(), i++)
+        tempmu.push_back(newlist[0]);
+        for(int i=1; i<newlist.size(); i++)
         {
             tempmu.push_back(newlist[i]);
-            allmus = recursiveLoop(indices, N, tempmu, allmus, endsize)
+            //for ( const auto &s : tempmu ) std::cout << s << ' ';
+            //std::cout << std::endl;
+            allmus = recursiveLoop(indices, N, tempmu, allmus, endsize);
         }
     }
-
     return allmus;
 }
 
-*/
 std::vector<std::vector<int>> perfectMatchingPairs(int N)
 {
-    std::vector<std::vector<int>> allmus;
+    std::vector<std::vector<int>> allmus{};
+
 
     if (N==0)
     {
@@ -68,14 +73,11 @@ std::vector<std::vector<int>> perfectMatchingPairs(int N)
     }
     else
     {
-     /*   int endsize{doublefactorial(N - 1)};
+        int endsize{doublefactorial(N - 1)};
         std::vector<int> listofmodes(N);
         std::iota(listofmodes.begin(), listofmodes.end(), 0);
-        allmus = rec_loop(listofmodes, N, std::vector<std::vector<int>> tempmu{},
-                            std::vector<std::vector<int>> allmus{}, endsize);
-    */
-
-        allmus = {{0,1},{0,2}};  // This is a tester line - needs to be deleted
+        std::vector<int> tempmu{}; // tried using the 'static' prefix
+        allmus = recursiveLoop(listofmodes, N, tempmu, allmus, endsize);
     }
 
     return allmus;
@@ -88,7 +90,7 @@ int main()
     // The program is going to produce various statistics associated with Gaussian Boson Sampling.
     // The first thing I am going to do is write the function that produces the perfect matching pairs.
 
-    int numberofmodes{6};
+    int numberofmodes{4};
     std::vector<std::vector<int>> PMP{perfectMatchingPairs(numberofmodes)};
     for ( const auto &row : PMP) //This is a method for printing 2D vectors from stackexchange
     {
